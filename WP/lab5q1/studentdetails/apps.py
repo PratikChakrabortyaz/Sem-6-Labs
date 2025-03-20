8dc90118ff5696d4329540c741f2b09170599a38
PratikChakrabortyaz <117933667+PratikChakrabortyaz@users.noreply.github.com>
2025-03-20 10:05:28 +0530

    Delete WP/lab5q1 directory

--- WP/lab5q1/studentdetails/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudentdetailsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'studentdetails'
--- WP/lab5q1/studentdetails/forms.py
+++
@@ -1,9 +0,0 @@
-# student_details/forms.py
-
-from django import forms
-from .models import Student
-
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ['name', 'dob', 'address', 'contact_number', 'email', 'english_marks', 'physics_marks', 'chemistry_marks']
